7e2d3a10c5ddae8e2be7e63baebf62423479adfd
Nikita Bendre <nickybendre898@gmail.com>
2024-11-14 22:06:07 +0530

    Style Binding

---
+++ src/app/marvellous/marvellous.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marvellous',
+  standalone: true,
+  imports: [],
+  templateUrl: './marvellous.component.html',
+  styleUrl: './marvellous.component.css'
+})
+export class MarvellousComponent 
+{
+  public Mycolor = "Orange";
+
+  public FoodPreference = false;
+
+}
